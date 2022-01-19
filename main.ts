b998a08bf98d3bd103263eb57dce63e36572e09e
Robert Herbig <rpherbig@sep.com>
2022-01-19 15:17:46 -0500

    Revert linter-required changes to main.ts to show a linting failure

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'path'
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
