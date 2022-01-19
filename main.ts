b8735ba0071610351b10ecdf362ed0d8c86d9ba1
Robert Herbig <rpherbig@sep.com>
2022-01-19 14:59:27 -0500

    Configure ESLint for TypeScript

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
 
 function createWindow () {
   const win = new BrowserWindow({
