10bc0b6857c9699bf06b3216e595ed7b7798713e
Jake Baker <jxbaker@sep.com>
2022-01-20 14:18:23 -0500

    Calculator tests working

--- Calculator.test.tsx
+++ Calculator.test.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Calculator from "./Calculator";
-import {render, screen} from '@testing-library/react'
+import { NumericInput } from './NumericInput';
+import TestRenderer, { act } from 'react-test-renderer';
+import { MathButton } from './MathButton';
+
 
 describe("Calculator app", () => {
-  // const expectedResult = 3;
-  // let capturedResult: number;
-  // const dummyOperation = (a: number, b: number) => expectedResult;
-  // const dummySetResult = (a: number) => { capturedResult = a};
 
   test("initial state", () => {
-    const {queryAllByTestId} = render(<Calculator/>);
-    const numInput1 = queryByTestId("firstOperand");
+    const calculator = TestRenderer.create(<Calculator/>);
+    const span = calculator.root.findByType("span");
+    expect(span.children[1]).toBe("0");
+    const ni = calculator.root.findAllByType(NumericInput);
+    expect(ni).toHaveLength(2);
+    expect(ni[0].props["getter"]).toBe(0);
+  });
+
+  test("buttons are connected", () => {
+    const calculator = TestRenderer.create(<Calculator/>);
+    const span = calculator.root.findByType("span");
+    const ni = calculator.root.findAllByType(NumericInput);
+    const buttons = calculator.root.findAllByType("button");
+    act(()=> {
+      ni[0].props["setter"](1);
+      ni[1].props["setter"](1);
+    });
+    act(() => {buttons[0].props["onClick"]()});
+    expect(span.children[1]).toBe("2");
   });
 });
