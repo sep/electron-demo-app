156f80c646cae836ab694ba9632f7d12f34741b0
Robert Herbig <rpherbig@sep.com>
2022-01-20 11:48:19 -0500

    Unit tests for MathButton component

---
+++ MathButton.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MathButton } from "./MathButton";
+import TestRenderer from 'react-test-renderer';
+
+describe("Math button", () => {
+  const expectedResult = 3;
+  let capturedResult: number;
+  const dummyOperation = (a: number, b: number) => expectedResult;
+  const dummySetResult = (a: number) => { capturedResult = a};
+
+  test("sets result of applying operation", () => {
+    const testRenderer = TestRenderer.create(
+      <MathButton
+        firstOperand={1}
+        secondOperand={2}
+        operation={dummyOperation}
+        setResult={dummySetResult}
+        label="+" />);
+    const testInstance = testRenderer.root;
+    testInstance.findByType("button").props["onClick"]();
+    expect(capturedResult).toEqual(expectedResult);
+  });
+});
--- MathButton.tsx
+++ MathButton.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export function MathButton({ firstOperand, secondOperand, operation, setResult, label }: any) {
+export interface MathButtonType {
+  firstOperand: number;
+  secondOperand: number;
+  operation: (a: number, b: number) => number;
+  setResult: (a: number) => void;
+  label: string;
+}
+
+export function MathButton({ firstOperand, secondOperand, operation, setResult, label }: MathButtonType) {
   const onClick = () => setResult(operation(firstOperand, secondOperand));
 
   return (
