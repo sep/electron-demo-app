81ce04e8a21187b01ab39b2ada51d7bfd07713ac
Robert Herbig <rpherbig@sep.com>
2022-01-20 06:54:10 -0500

    Use IPC API for main <-> renderer communication

--- main.ts
+++ main.ts
@@ -1,30 +1,35 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'path'
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "path";
+import fs from "fs";
 
-function createWindow () {
-  const win = new BrowserWindow({
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win: BrowserWindow;
+
+async function createWindow() {
+
+  // Create the browser window.
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      // enableRemoteModule: false, // turn off remote
+      preload: path.join(__dirname, "preload.js") // use a preload script
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  // Load app
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // rest of code..
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.on("ready", createWindow);
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+ipcMain.on("toMain", (event, args) => {
+  // Send result back to renderer process
+  win.webContents.send("fromMain", "this is what I'm sending to the renderer");
+  //fs.readFile("path/to/file", (error, data) => {/*do something*/});
+});
--- preload.ts
+++ preload.ts
@@ -1,11 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+import { contextBridge, ipcRenderer } from "electron";
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    const version = process.versions[type] || "not found"
-    replaceText(`${type}-version`, version)
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  "api", {
+      send: (channel: string, data: any) => {
+          // whitelist channels
+          const validChannels = ["toMain"];
+          if (validChannels.includes(channel)) {
+              ipcRenderer.send(channel, data);
+          }
+      },
+      receive: (channel: string, func: (...args: any) => void) => {
+          const validChannels = ["fromMain"];
+          if (validChannels.includes(channel)) {
+              // Deliberately strip event as it includes `sender` 
+              ipcRenderer.on(channel, (event, ...args) => func(...args));
+          }
+      }
   }
-})
+);
