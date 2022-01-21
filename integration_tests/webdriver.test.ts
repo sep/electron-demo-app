b87a244b8bc3595a979d526d764a5391f9b1c064
Jake Baker <jxbaker@sep.com>
2022-01-21 10:18:49 -0500

    Start chromedriver from within integration test

--- integration_tests/webdriver.test.ts
+++ integration_tests/webdriver.test.ts
@@ -1,23 +1,38 @@
 import webdriver, { ThenableWebDriver } from 'selenium-webdriver'
+import { ChildProcessWithoutNullStreams, exec } from "child_process";
 
 let driver : ThenableWebDriver = {} as ThenableWebDriver
+let child_process : ChildProcessWithoutNullStreams = {} as ChildProcessWithoutNullStreams
 
-beforeAll (() => {
-  driver = new webdriver.Builder()
-  // The "9515" is the port opened by chrome driver.
-  .usingServer('http://localhost:9515')
-  .withCapabilities({
-    'goog:chromeOptions': {
-      // Here is the path to your Electron binary.
-      binary: './out/electron-demo-app-win32-x64/electron-demo-app.exe'
-    }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+beforeAll ((done) => {
+  child_process = exec("npx chromedriver")
+  sleep(2000).then(() => {
+    driver = new webdriver.Builder()
+    // The "9515" is the port opened by chrome driver.
+    .usingServer('http://localhost:9515')
+    .withCapabilities({
+      'goog:chromeOptions': {
+        // Here is the path to your Electron binary.
+        binary: './out/electron-demo-app-win32-x64/electron-demo-app.exe'
+      }
+    })
+    .forBrowser('chrome') // note: use .forBrowser('electron') for selenium-webdriver <= 3.6.0
+    .build()
+    done();
   })
-  .forBrowser('chrome') // note: use .forBrowser('electron') for selenium-webdriver <= 3.6.0
-  .build()
 })
 
-afterAll (() => {
+afterAll ((done) => {
   driver.quit()
+  child_process.kill('SIGINT')
+  while (child_process.killed) {
+    // do nothing
+  }
+  done();
 });
 
 
