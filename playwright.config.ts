7d7ef79d2af122fe747175ffb4e06a3ac152b1a7
Robert Herbig <rpherbig@sep.com>
2022-01-24 09:30:34 -0500

    Config to run Playwright in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   testDir: './integration_tests/playwright',
   maxFailures: 2,
+  use: {
+    headless: true
+  }
 }
 
 export default config
